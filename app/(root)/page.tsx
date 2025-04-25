341a37bdb02d4a143aff0e3e1cea78ee379d12a1
lalitshrotriya2002 <lalitshrotriya2002@gmail.com>
2025-04-25 19:25:55 +0000

    Final-Commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -25,9 +25,9 @@ async function Home() {
     <>
       <section className="card-cta">
         <div className="flex flex-col gap-6 max-w-lg">
-          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+          <h2>Get Interview-Ready with AI-Powered Questioning & Feedback</h2>
           <p className="text-lg">
-            Practice real interview questions & get instant feedback
+            Face Real Interview Questions & get instant feedback
           </p>
 
           <Button asChild className="btn-primary max-sm:w-full">
